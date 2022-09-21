ab88e84eb69cc2fce8fecd3a7914144da2ae8b06
efeokus <efeokujeni@gmail.com>
2022-09-21 02:35:17 -0700

    updated

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=database-1.cu3dqlme7joa.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=mydatabase.caqepxfks0wt.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=efeawsbucket
-export AWS_REGION=us-east-1
+export AWS_BUCKET=efeoawsbucket
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
